b651f87f6f1c91ad2ebe7d7798dad1c0024a71f3
warnov <warnov@gmail.com>
2018-12-08 21:52:15 -0500

    Working in big blob reading

---
+++ BlobWriter.Tester/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobWriter.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var account = CloudStorageAccount.Parse("AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1");
+            var blobClient = account.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("acme");
+            var blob = container.GetBlobReference("acme081218.select");
+            byte[] content=new byte[800];
+            blob.DownloadToByteArray(content, index: 0);
+            //var content = blob..DownloadText();
+
+
+            //Console.WriteLine(content.Substring(0, 500));
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+ * 
+ * {
+  "IsEncrypted": false,
+  "Values": {
+    "AzureWebJobsStorage": "UseDevelopmentStorage=true",
+    "FUNCTIONS_WORKER_RUNTIME": "dotnet",
+    "blobConnectionString": "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1",
+    "batchSize": "100"
+  }
+}
+
+    */
--- BlobWriter/OriginBlobReader.cs
+++ BlobWriter/OriginBlobReader.cs
@@ -5,57 +5,64 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace FileTransformer
 {
     public static class BlobReader
     {
+
+        /*
+         * POST must be like:
+         * {
+         *     "originBlobName" : "acme081218.select",
+               "originContainerName" : "acme",
+               "destinyContainerName" : "acme_batchs",
+               "destinyBlobRootName" : "acmebatch"
+           }
+           */
+
         [FunctionName("BlobReader")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log, ExecutionContext context)
         {
 
+            #region Reading conf and params
+        
             var config = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
+                        .SetBasePath(context.FunctionAppDirectory)
+                        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables()
+                        .Build();
+
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            //Reading original Blob Path from post data
-            string originBlobName, originContainerName;
+            //Reading parameters from post data
+            string originBlobName, originContainerName, destinyContainerName, destinyBlobRootName;
             try
             {
                 originBlobName = data.originBlobName;
                 originContainerName = data.originContainerName;
+                destinyContainerName = data.destinyContainerName;
+                destinyBlobRootName = data.destinyBlobRootName;
             }
             catch
             {
                 return CrossUtils.ReturnException("Bad parameters", log);
             }
-
-
-            string fileContent;
-            var blobConnectionString = config["blobConnectionString"];
-            var writerFunctionCode = config["writerFunctionCode"];
-            WebClient wc = new WebClient();
-            wc.Headers.Add("code", writerFunctionCode);
-
-            List<string> bills = null;
-            int batchSize = 0;
-            Uri writerFunctionURI = null;
+           
+            int batchSize = 0;            
             try
             {
-                writerFunctionURI = new Uri(config["writerFunctionURL"]);
+                //writerFunctionURI = new Uri(config["writerFunctionURL"]);
                 batchSize = int.Parse(config["batchSize"]);
             }
             catch
@@ -63,6 +70,9 @@ namespace FileTransformer
                 return CrossUtils.ReturnException("Invalid parameters", log);
             }
 
+            var blobConnectionString = config["blobConnectionString"];
+            var billsCount = 0;
+            #endregion
 
 
             if (CloudStorageAccount.TryParse(blobConnectionString, out CloudStorageAccount cloudStorageAccount))
@@ -70,33 +80,47 @@ namespace FileTransformer
                 var blobClient = cloudStorageAccount.CreateCloudBlobClient();
 
 
-                //Getting origin blob content
+                //Processing origin blob into batches of lists
                 try
                 {
                     var originContainer = blobClient.GetContainerReference(originContainerName);
                     var originBlob = originContainer.GetBlockBlobReference(originBlobName);
-                    fileContent = await originBlob.DownloadTextAsync();
-                    bills = new List<string>(fileContent.Split("FINDOCUM"));
-                    for (int i = 0; i < bills.Count; i += batchSize)
+                    BlobContinuationToken blobContinuationToken = null;
+                    var listb = await originContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                    blobContinuationToken = listb.ContinuationToken;
+                    var fileContent = await originBlob.DownloadTextAsync();
+                    var bills = new List<string>(fileContent.Split("FINDOCUM"));
+                    billsCount = bills.Count;
+                    for (int i = 0, batchNumber = 0; i < billsCount; i += batchSize, batchNumber++)
                     {
-                        var batchDataList = bills.GetRange(i, batchSize);
-                        var batchDataString = JsonConvert.SerializeObject(batchDataList);
-                        wc.UploadStringAsync(writerFunctionURI, batchDataString);
-                    }
+                        var offset = i + batchSize > billsCount ? billsCount - i : batchSize;
+                        var batchDataList = bills.GetRange(i, offset);
+                        var batchDataJsonString = JsonConvert.SerializeObject(batchDataList);
+                        ProcessBatchDataString(batchDataJsonString, batchNumber,
+                            blobClient, destinyContainerName, destinyBlobRootName);
+                    }                    
                 }
-                catch
+                catch(Exception exc)
                 {
                     return CrossUtils.ReturnException("Problem reading origin blob", log);
                 }
-
             }
             else
             {
                 return CrossUtils.ReturnException("Bad connection string", log);
             }
 
-            return new OkObjectResult($"{bills?.Count} bills processed");
+            return new OkObjectResult($"{billsCount} bills processed");
         }
 
+        private static async void ProcessBatchDataString(string batchDataJsonString, int batchNumber, 
+            CloudBlobClient blobClient, string destinyContainerName, string destinyBlobRootName)
+        {
+            var container = blobClient.GetContainerReference(destinyContainerName);
+            await container.CreateIfNotExistsAsync();
+            var blobName = $"{batchNumber}-{destinyBlobRootName}.json";
+            var blob = container.GetBlockBlobReference(blobName);
+            _ = blob.UploadTextAsync(batchDataJsonString);
+        }
     }
 }
